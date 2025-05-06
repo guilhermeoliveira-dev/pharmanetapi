2eaa255a4c14e3d8fc860c474af4e0a543531a5d
guilhermeoliveira-dev <guilhermeovergame@gmail.com>
2025-05-06 08:01:47 -0300

    DTO Incompleto

---
+++ src/main/java/com/pixelguardian/pharmanetapi/api/dto/CategoriaDTO.java
@@ -0,0 +1,15 @@
+package com.pixelguardian.pharmanetapi.api.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoriaDTO {
+
+
+
+}
