ca0a192d8614b16e8f04cdec8003f24dcf3160e1
jhuan <souzajhuan11@gmail.com>
2025-05-29 08:16:02 -0300

    Controller incompleto

--- src/main/java/com/pixelguardian/pharmanetapi/api/controller/FarmaciaController.java
+++ src/main/java/com/pixelguardian/pharmanetapi/api/controller/FarmaciaController.java
@@ -4,13 +4,18 @@ import com.pixelguardian.pharmanetapi.api.dto.FarmaciaDTO;
 import com.pixelguardian.pharmanetapi.model.entity.Endereco;
 import com.pixelguardian.pharmanetapi.model.entity.Farmacia;
 import com.pixelguardian.pharmanetapi.service.EnderecoService;
+import com.pixelguardian.pharmanetapi.service.FarmaciaService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/farmacias")
@@ -18,7 +23,14 @@ import java.util.Optional;
 @CrossOrigin
 public class FarmaciaController {
 
+    private final FarmaciaService service;
     private final EnderecoService enderecoService;
+
+    @GetMapping()
+    public ResponseEntity get() {
+        List<Farmacia> farmacias = service.getFarmacias();
+        return ResponseEntity.ok(farmacias.stream().map(FarmaciaDTO::create).collect(Collectors.toList()));
+    }
 
     public Farmacia converter(FarmaciaDTO dto) {
         ModelMapper modelMapper = new ModelMapper();
